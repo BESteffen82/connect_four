2d2df42b3203e886010154815b025fa82587c978
Brent Steffen <BESteffen82@gmail.com>
2022-07-13 16:31:49 -0400

    Add ConnectFour test method

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,5 +1,8 @@
-require './connect_four'
+require_relative '../lib/connect_four'
+require_relative '../lib/player'
 
 describe ConnectFour do
   subject(:game) { described_class.new }
+	let(:player){ double(Player) }
+
 end
