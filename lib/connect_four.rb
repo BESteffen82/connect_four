eec9fa7d48b8fa4f24bd734c802d51947f4309ed
Brent Steffen <BESteffen82@gmail.com>
2022-07-13 13:04:10 -0400

    Initial commit

---
+++ lib/connect_four.rb
@@ -0,0 +1,14 @@
+class ConnectFour
+	attr_accessor
+
+	EMPTY_CIRCLE = "\e[37m\u25cb".freeze
+	YELLOW_CIRCLE = "\u001b[33m\u25cf".freeze
+	RED_CIRCLE = "\u001b[31m\u25cf".freeze
+
+	def initialize
+		@board = Array.new
+		@player_one = 
+		@player_two = 
+	end
+
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,5 @@
+require './connect_four'
+
+describe ConnectFour do
+  subject(:game) { described_class.new }
+end
